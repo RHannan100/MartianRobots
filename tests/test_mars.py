d7a2ab38f1e9881304f725c9a265eac1aa749cd7
Robert Hannan <rhannan100@gmail.com>
2022-02-19 15:58:00 +0000

    Add empty requirement tests

---
+++ tests/test_mars.py
@@ -0,0 +1,19 @@
+import pytest
+
+def test_maximum_grid_is_50x50():
+    assert False
+
+def test_maximum_instruction_string_is_less_than_100():
+    assert False
+
+def test_robot_created_at_input_gridpoint():
+    assert False
+
+def test_robot_created_for_each_instruction_pair():
+    assert False
+
+def test_robot_location_output_after_moving():
+    assert False
+
+def test_LOST_and_final_location_output_after_moving_oob():
+    assert False
---
+++ tests/test_robot.py
@@ -0,0 +1,36 @@
+import pytest
+
+def test_orientation_limits():
+    # Test that only NESW are valid orientations
+    assert False
+
+def test_instruction_limits():
+    # Test that only LRF are valid instructions
+    assert False
+
+def test_l_instruction_stays_on_current_gridpoint():
+    assert False
+
+def test_r_instruction_stays_on_current_gridpoint():
+    assert False
+
+def test_f_instruction_maintains_orientation():
+    assert False
+
+def test_f_instruction_increments_y_axis_when_facing_north():
+    assert False
+
+def test_f_instruction_increments_x_axis_when_facing_east():
+    assert False
+
+def test_f_instruction_decrements_y_axis_when_facing_south():
+    assert False
+
+def test_f_instruction_decrements_x_axis_when_facing_west():
+    assert False
+
+def test_leaving_grid_marks_robot_lost():
+    assert False
+
+def test_last_gridpoint_marked_when_robot_lost():
+    assert False
