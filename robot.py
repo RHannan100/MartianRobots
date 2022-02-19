3b2f431e689d3a44e3783510f923023cd4863352
Robert Hannan <rhannan100@gmail.com>
2022-02-19 17:16:19 +0000

    Add basic robot passing movement tests

---
+++ robot.py
@@ -0,0 +1,64 @@
+from enum import Enum
+
+ROTATIONS = {
+    "L": -1,
+    "R": 1
+}
+
+class Orientation(Enum):
+    N = 0
+    E = 1
+    S = 2
+    W = 3
+
+class Robot:
+    """A robot for use on Mars"""
+
+    orientation = Orientation.N
+    position_x = 0
+    position_y = 0
+    lost = False
+
+    def set_orientation(self, orientation):
+        self.orientation = Orientation[orientation]
+
+    def get_orientation(self):
+        return self.orientation.name
+
+    def get_location(self):
+        return {"x": self.position_x, "y": self.position_y}
+
+    def rotate(self, direction):
+        new_orientation = self.orientation.value + ROTATIONS[direction]
+
+        if new_orientation == -1:
+            new_orientation = 3
+        elif new_orientation == 4:
+            new_orientation = 0
+        
+        self.orientation = Orientation(new_orientation)
+        return self.position_x, self.position_y
+
+    def forward(self, amount):
+        if self.orientation is Orientation.N:
+            self.position_y += amount
+            return self.get_location()
+        elif self.orientation is Orientation.E:
+            self.position_x += amount
+            return self.get_location()
+        elif self.orientation is Orientation.S:
+            self.position_y -= amount
+            return self.get_location()
+        elif self.orientation is Orientation.W:
+            self.position_x -= amount
+            return self.get_location()
+        else:
+            return -1
+
+    def move(self, instruction):
+        if instruction == "F":
+            self.forward(1)
+        elif instruction in ["L", "R"]:
+            self.rotate(instruction)
+        else:
+            return -1
--- tests/test_robot.py
+++ tests/test_robot.py
@@ -1,12 +1,17 @@
 import pytest
 
-def test_orientation_limits():
-    # Test that only NESW are valid orientations
-    assert False
+from robot import Robot
 
 def test_instruction_limits():
-    # Test that only LRF are valid instructions
-    assert False
+    robot = Robot()
+    assert robot.move("F") != -1
+    assert robot.move("L") != -1
+    assert robot.move("R") != -1
+    assert robot.move("Q") == -1
+    assert robot.move("W") == -1
+    assert robot.move("1") == -1
+    assert robot.move(1) == -1
+    assert robot.move(["F"]) == -1
 
 def test_l_instruction_stays_on_current_gridpoint():
     robot = Robot()
@@ -53,30 +58,30 @@ def test_f_instruction_maintains_orientation():
 def test_f_instruction_increments_y_axis_when_facing_north():
     robot = Robot()
     robot.set_orientation("N")
-    initial_y = robot.get_location()['y']
+    initial_y = robot.get_location()["y"]
     robot.move("F")
-    assert (initial_y + 1) == robot.get_location()['y']
+    assert (initial_y + 1) == robot.get_location()["y"]
 
 def test_f_instruction_increments_x_axis_when_facing_east():
     robot = Robot()
     robot.set_orientation("E")
-    initial_x = robot.get_location()['x']
+    initial_x = robot.get_location()["x"]
     robot.move("F")
-    assert (initial_x + 1) == robot.get_location()['x']
+    assert (initial_x + 1) == robot.get_location()["x"]
 
 def test_f_instruction_decrements_y_axis_when_facing_south():
     robot = Robot()
     robot.set_orientation("S")
-    initial_y = robot.get_location()['y']
+    initial_y = robot.get_location()["y"]
     robot.move("F")
-    assert (initial_y - 1) == robot.get_location()['y']
+    assert (initial_y - 1) == robot.get_location()["y"]
 
 def test_f_instruction_decrements_x_axis_when_facing_west():
     robot = Robot()
     robot.set_orientation("W")
-    initial_x = robot.get_location()['x']
+    initial_x = robot.get_location()["x"]
     robot.move("F")
-    assert (initial_x - 1) == robot.get_location()['x']
+    assert (initial_x - 1) == robot.get_location()["x"]
 
 def test_leaving_grid_marks_robot_lost():
     assert False
